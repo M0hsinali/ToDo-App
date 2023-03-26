6e8fe87c92acd537b7c80ce9dda9c98511f89c8a
Mohsin Ali <mohsinbalochali56@gmail.com>
2023-03-27 02:20:01 +0500

    Day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:learning/Pages/login_page.dart';
+
 import 'Pages/home_page.dart';
 
 void main() {
@@ -11,6 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      //home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.amber),
+      routes: {"/": (context) => LoginPage(), "/home": (context) => HomePage()},
+    );
   }
 }
